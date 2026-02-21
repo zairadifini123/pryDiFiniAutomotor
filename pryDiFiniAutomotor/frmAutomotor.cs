de0bff7a477005f8a8f1b35c83149d1e579e78c3
Zaira Di Fini <zairadifini123@gmail.com>
2026-02-21 17:00:44 -0300

    Agregar archivos de proyecto.

---
+++ pryDiFiniAutomotor/frmAutomotor.cs
@@ -0,0 +1,10 @@
+namespace pryDiFiniAutomotor
+{
+    public partial class frmAutomotor : Form
+    {
+        public frmAutomotor()
+        {
+            InitializeComponent();
+        }
+    }
+}
